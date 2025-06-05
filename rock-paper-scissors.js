3d9412623e0291a918234384301e730d31786176
RstK <208619198+bowman025@users.noreply.github.com>
2025-06-05 15:11:34 +0200

    Write choice functions and add score variables

---
+++ rock-paper-scissors.js
@@ -0,0 +1,29 @@
+function getComputerChoice() {
+    if (Math.floor(Math.random() * 3) === 0)
+        return("Rock");
+    else if (Math.floor(Math.random() * 3) === 1)
+        return("Paper");
+    else 
+        return("Scissors");
+    }
+
+console.log(getComputerChoice());
+
+sign = prompt("Pick One: Rock, Paper or Scissors");
+
+function getHumanChoice(prompt) {
+    if (sign === "Rock")
+        return("Rock");
+    if (sign === "Paper")
+        return("Paper");
+    if (sign === "Scissors")
+        return("Scissors");
+    else
+        return("Invalid Response");
+}
+
+console.log(getHumanChoice())
+
+let computerScore = 0;
+let humanScore = 0;
+
